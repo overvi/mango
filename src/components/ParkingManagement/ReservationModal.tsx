3c9f73444d01b56dd7ce21da73eaa4f03643f834
overvi <aliwebdevf@gmail.com>
2025-10-01 12:49:07 +0330

    a

--- src/components/ParkingManagement/ReservationModal.tsx
+++ src/components/ParkingManagement/ReservationModal.tsx
@@ -1,18 +1,16 @@
-import Car from "@/assets/images/car-outline.svg?react";
-import StatusTag from "./StatusTag";
 import { STATUS_LABELS } from "@/constants/statusLabels";
-import Close from "@/assets/images/close-circle.svg?react";
 
-import Tabs, { type Tab } from "../Tab/Tab";
-import Clock from "@/assets/images/clock.svg?react";
 import CarFilled from "@/assets/images/car.svg?react";
+import Clock from "@/assets/images/clock.svg?react";
 import Danger from "@/assets/images/danger.svg?react";
+import { useState } from "react";
+import Tabs, { type Tab } from "../Tab/Tab";
+import FailureHint from "./FailureHint";
+import FastSearch from "./FastSearch";
+import RecordFailure from "./RecordFailure";
 import RegistrationStep from "./RegistrationStep";
 import ReserveForLater from "./ReserveForLater";
-import RecordFailure from "./RecordFailure";
-import { useState } from "react";
-import FastSearch from "./FastSearch";
-import FailureHint from "./FailureHint";
+import StatusModalHeader from "./StatusModalHeader";
 
 const tabs: Tab[] = [
   {
@@ -41,23 +39,7 @@ const ReservationModal = () => {
 
   return (
     <div className=" w-[31.375rem] p-6 ">
-      <div className="flex justify-between border-b border-b-gray-350 dark:border-b-gray-800 pb-5">
-        <div className="flex items-center gap-2 text-sm">
-          <div className="bg-neutral-50 text-orange-500 flex items-center justify-center size-15 rounded-[.875rem]">
-            <Car width={30} height={30} />
-          </div>
-          <div className="flex  flex-col text-sm">
-            <div className="flex items-center gap-4">
-              <p>جایگاه ۷</p>
-              <StatusTag status={STATUS_LABELS.RESERVED} />
-            </div>
-            <p className="text-gray-650 mt-2 ">بخش A طبقه ۱</p>
-          </div>
-        </div>
-        <button className="self-start">
-          <Close />
-        </button>
-      </div>
+      <StatusModalHeader type={STATUS_LABELS.RESERVED} />
       {activeTab !== "3" ? <FastSearch /> : <FailureHint />}
       <Tabs activeTab={activeTab} setActiveTab={setActiveTab} tabs={tabs} />
     </div>
--- src/components/ParkingManagement/StatusModalHeader.tsx
+++ src/components/ParkingManagement/StatusModalHeader.tsx
@@ -2,11 +2,13 @@ import { STATUS_LABELS, STATUS_STYLES } from "@/constants/statusLabels";
 import StatusTag from "./StatusTag";
 import Car from "@/assets/images/car-outline.svg?react";
 import Close from "@/assets/images/close-circle.svg?react";
+import useModal from "../Modal/useModal";
 
 interface Props {
   type: (typeof STATUS_LABELS)[keyof typeof STATUS_LABELS];
 }
 const StatusModalHeader = ({ type }: Props) => {
+  const { closeModal } = useModal();
   return (
     <div className="flex justify-between border-b border-b-gray-350 dark:border-b-gray-800  pb-5">
       <div className="flex items-center gap-2 text-sm">
@@ -24,7 +26,7 @@ const StatusModalHeader = ({ type }: Props) => {
           <p className="text-gray-650 mt-2 ">بخش A طبقه ۱</p>
         </div>
       </div>
-      <button className="self-start">
+      <button onClick={() => closeModal()} className="self-start">
         <Close />
       </button>
     </div>
